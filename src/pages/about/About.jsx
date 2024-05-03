8e7381b2cd6186e7c4dca09f6e3d0ee14e726435
gabrielteza <gabrielcarbonitezaa@gmail.com>
2024-05-03 13:36:24 -0300

    "Updated About and Projetos pages with improved styling and responsiveness"

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -7,68 +7,54 @@ export default function About() {
     <div className="about" id="about">
       <div className="container-about">
         <div className="container-box">
-        <div className="about-text">
-          <h1
-            style={{ fontWeight: "bolder", fontSize: "4rem", color: "#cfc37c" }}
-          >
-            {" "}
-            <Typewriter
-              words={["Olá! Meu nome é Gabriel", "Desenvolvedor Web"]}
-              loop={0}
-              cursor
-              cursorStyle="<"
-              typeSpeed={100}
-              deleteSpeed={80}
-              delaySpeed={1000}
-            />
-          </h1>
-          <br />
-          <p className="about-text-p"
-          >
-            Tenho 18 anos e sou apaixonado por programação.
-          </p>
-          <p
-           className="about-text-p"
-          >
-            Desde que me lembro, a tecnologia sempre me fascinou e aprender a
-            criar software tornou-se uma das minhas paixões.
-          </p>
-          <p
-            className="about-text-p"
-          >
-            {" "}
-            Atualmente, estou focado em desenvolver minhas habilidades como
-            programador, buscando constantemente aprender novas tecnologias e
-            aprimorar meus conhecimentos existentes. Estou determinado a
-            transformar minha paixão em uma carreira sólida e gratificante em
-            tecnologia.
-          </p>{" "}
-          <p
-           className="about-text-p"
-          >
-            {" "}
-            Meu maior sonho é conseguir meu primeiro emprego profissional na
-            área de programação.
-          </p>{" "}
-          <p
-           className="about-text-p"
-          >
-            Estou empenhado em trabalhar duro para atingir esse objetivo,
-            buscando oportunidades de estágio ou emprego onde possa aplicar
-            minhas habilidades e continuar aprendendo e crescendo
-            profissionalmente.
-          </p>{" "}
-          <p
-           className="about-text-p"
-          >
-            Estou animado para ver o que o futuro reserva para minha jornada
-            profissional de programação e estou pronto para os desafios que
-            surgirão pelo caminho.
-          </p>
+          <div className="about-text">
+            <h1 className="about-text-h1">
+              {" "}
+              <Typewriter
+                words={["Olá! Meu nome é Gabriel", "Desenvolvedor Web"]}
+                loop={0}
+                cursor
+                cursorStyle="<"
+                typeSpeed={100}
+                deleteSpeed={80}
+                delaySpeed={1000}
+              />
+            </h1>
+            <br />
+            <p className="about-text-p">
+              Tenho 18 anos e sou apaixonado por programação.
+            </p>
+            <p className="about-text-p">
+              Desde que me lembro, a tecnologia sempre me fascinou e aprender a
+              criar software tornou-se uma das minhas paixões.
+            </p>
+            <p className="about-text-p">
+              {" "}
+              Atualmente, estou focado em desenvolver minhas habilidades como
+              programador, buscando constantemente aprender novas tecnologias e
+              aprimorar meus conhecimentos existentes. Estou determinado a
+              transformar minha paixão em uma carreira sólida e gratificante em
+              tecnologia.
+            </p>{" "}
+            <p className="about-text-p">
+              {" "}
+              Meu maior sonho é conseguir meu primeiro emprego profissional na
+              área de programação.
+            </p>{" "}
+            <p className="about-text-p">
+              Estou empenhado em trabalhar duro para atingir esse objetivo,
+              buscando oportunidades de estágio ou emprego onde possa aplicar
+              minhas habilidades e continuar aprendendo e crescendo
+              profissionalmente.
+            </p>{" "}
+            <p className="about-text-p">
+              Estou animado para ver o que o futuro reserva para minha jornada
+              profissional de programação e estou pronto para os desafios que
+              surgirão pelo caminho.
+            </p>
+          </div>
         </div>
       </div>
-        </div>
-       
     </div>
   );
 }
--- src/pages/projetos/Projetos.jsx
+++ src/pages/projetos/Projetos.jsx
@@ -5,8 +5,6 @@ import gerador from "../../img/gerador.png";
 import vpn from "../../img/vpn.PNG";
 import { Link } from "react-scroll";
 
-// The Main Function
-
 export default function Projetos() {
   return (
     <div className="projetos" id="projetos">
@@ -14,95 +12,53 @@ export default function Projetos() {
         <div className="titulo-projetos">
           <h1>Projetos</h1>
           <div className="grid-projetos">
-            <div className="grid-1">
+            <div className="grid-item">
               <div className="grid-titulo">
                 <p>Tezadmin</p>
                 <img src={adm} alt="" className="img" />
                 <div className="desc-container">
-                  <span id="text">Sistema de administrador feito em ReactJs, para vizualizar Lucros, Vendas, Custos, Usuários ativos por mês, Dados dos usuários.</span>
-
+                  <span className="text">Sistema de administrador feito em <mark id="marker-text">ReactJs</mark>, para visualizar Lucros, Vendas, Custos, Usuários ativos por mês, Dados dos usuários.</span>
                 </div>
                 <div className="container-btn">
                   <button className="btn-project">
-                    <a
-                      href="https://github.com/gabrielteza/tezadmin"
-                      target="_blank"
-                      className="btn-project"
-                    >
-                      Repositorio GitHub
-                    </a>
+                    <a href="https://github.com/gabrielteza/tezadmin" target="_blank" className="btn-project">Repositório GitHub</a>
                   </button>
                   <button className="btn-project">
-                    <a
-                      href="https://tezadmin.vercel.app/"
-                      target="_blank"
-                      className="btn-project"
-                    >
-                      Ver projeto
-                    </a>
+                    <a href="https://tezadmin.vercel.app/" target="_blank" className="btn-project">Ver projeto</a>
                   </button>
                 </div>
               </div>
             </div>
-            <div className="grid-2">
+            <div className="grid-item">
               <div className="grid-titulo">
                 <p>Gerador de senha</p>
-                <img src={gerador} alt="" className="img2" />
+                <img src={gerador} alt="" className="img" />
                 <div className="desc-container">
-                  <span id="text2">Este projeto foi feito em HTML, CSS e Javascript, Gerador de Senhas Aleatorias.</span>
-
+                  <span className="text">Este projeto foi feito em <mark id="marker-text">HTML, CSS e JavaScript</mark>, gerador de senhas aleatórias.</span>
                 </div>
                 <div className="container-btn">
-                  <button className="btn-project2">
-                    <a
-                      href="https://github.com/gabrielteza/geredor-de-senha"
-                      target="_blank"
-                      className="btn-project2"
-                    >
-                      Repositorio GitHub
-                    </a>
+                  <button className="btn-project">
+                    <a href="https://github.com/gabrielteza/geredor-de-senha" target="_blank" className="btn-project">Repositório GitHub</a>
                   </button>
-                  <button className="btn-project2">
-                    <a
-                      href="https://geredor-de-senha.vercel.app/"
-                      target="_blank"
-                      className="btn-project2"
-                    >
-                      Ver projeto
-                    </a>
+                  <button className="btn-project">
+                    <a href="https://geredor-de-senha.vercel.app/" target="_blank" className="btn-project">Ver projeto</a>
                   </button>
                 </div>
               </div>
             </div>
-            <div className="grid-3">
+            <div className="grid-item">
               <div className="grid-titulo">
                 <p>Vpn Landing Page</p>
-                <img src={vpn} alt="" className="img3" />
+                <img src={vpn} alt="" className="img" />
                 <div className="desc-container">
-                  <span id="text3">
-                    Este projeto foi feito em ReactJs, com inspiração em um desgin
-                    do figma.
-                  </span>
+                  <span className="text">Este projeto foi feito em <mark id="marker-text">ReactJs</mark>, com inspiração em um design do Figma.</span>
                 </div>
-
                 <div className="container-btn">
-                  <button className="btn-project3">
-                    <a
-                      href="https://github.com/gabrielteza/vpnlandingpage"
-                      target="_blank"
-                      className="btn-project3"
-                    >
-                      Repositorio GitHub
-                    </a>
+                  <button className="btn-project">
+                    <a href="https://github.com/gabrielteza/vpnlandingpage" target="_blank" className="btn-project">Repositório GitHub</a>
                   </button>
-                  <button className="btn-project3">
-                    <a
-                      href="https://vpnlandingpage.vercel.app/"
-                      target="_blank"
-                      className="btn-project3"
-                    >
-                      Ver projeto
-                    </a>
+                  <button className="btn-project">
+                    <a href="https://vpnlandingpage.vercel.app/" target="_blank" className="btn-project">Ver projeto</a>
                   </button>
                 </div>
               </div>
