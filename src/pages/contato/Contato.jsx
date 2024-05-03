6bf119a061c31715c936548451834b941e4328f8
gabrielteza <gabrielcarbonitezaa@gmail.com>
2024-05-03 15:16:20 -0300

    add contact form on page, in construction

--- src/pages/contato/Contato.jsx
+++ src/pages/contato/Contato.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './contato.css'
+import ContactForm from '../../components/contactForm/ContactForm'
 
 export default function Contato() {
   return (
     <div className='contato' id='contato'>
       <div className="container-contato">
-        
+      <ContactForm />
       </div>
     </div>
   )
